bd90682b2a8de6801562f60a65dcdbf0bffe8795
Beş <89789101+Bes-js@users.noreply.github.com>
2022-09-13 22:48:27 +0300

    Update tag.js

--- src/beş_commands/Kayıt_Komutları/tag.js
+++ src/beş_commands/Kayıt_Komutları/tag.js
@@ -4,4 +4,4 @@ const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, Modal, TextInputBuilder, 
 const beş_config = require("../../../beş_config.json")
 module.exports = {name: "tag",aliases: ["tag"],execute: async (client, message, args, beş_embed) => {
 let member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
-if(member){message.reply({ content: `> **${beş_config.userTag ? beş_config.userTag[0] : "5"} ${member.user.username}${beş_config.etiketTag ? `#${beş_config.etiketTag[0]}` : "0005"}**`}) }else if(!member){message.reply({ content: `> **${beş_config.userTag ? beş_config.userTag : "5"}**`}) }}}
+if(member){message.reply({ content: `> **${beş_config.userTag ? beş_config.userTag[0] : "5"} ${member.user.username}${beş_config.etiketTag ? `#${beş_config.etiketTag[0]}` : "0005"}**`}) }else if(!member){message.reply({ content: `> **${beş_config.userTag ? beş_config.userTag : "5"} ${beş_config.etiketTag ? `#${beş_config.etiketTag}` :""}**`}) }}}
