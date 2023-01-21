9853063c86ecbe2b2f82d2dc7a286459d6f70804
Five So Beş <89789101+Bes-js@users.noreply.github.com>
2023-01-21 09:22:22 +0300

    Update isimler.js

--- src/beş_commands/Kayıt_Komutları/isimler.js
+++ src/beş_commands/Kayıt_Komutları/isimler.js
@@ -4,7 +4,7 @@ const moment = require("moment")
 require('moment-duration-format');
 const beş_config = require("../../../beş_config.json")
 module.exports = {name: 'isimler',aliases: ["names", "nicknames"],execute: async (client, message, args, beş_embed) => {
-var member = message.mentions.users.first() || message.guild.members.cache.get(args[0])
+var member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
 if (!message.member.roles.cache.has(beş_config.staffRole) && !message.member.permissions.has("ADMINISTRATOR")) return message.reply({ embeds: [beş_embed.setDescription(`> **Komutu Kullanmak İçin Yetkin Bulunmamakta!**`)] }).then((beş) => setTimeout(() => { beş.delete(); }, 10000));
 if (!member) return message.reply({ embeds: [beş_embed.setDescription(`> **Geçerli Bir User Belirt!**`)] }).catch((err) => console.log(err), client.tick(message)).then((e) => setTimeout(() => { e.delete(); }, 10000));
 let names = db.get(`isimler_${member.id}`);
