4a32c6c1d59f76b3d3e147b6fc833c228fcf830f
Five So Beş <89789101+Bes-js@users.noreply.github.com>
2023-01-21 09:30:46 +0300

    Update commandUpdate.js

--- src/beş_events/commandUpdate.js
+++ src/beş_events/commandUpdate.js
@@ -7,7 +7,7 @@ if (beş_config.prefix && !newMessage.content.startsWith(beş_config.prefix)) re
 const args = newMessage.content.slice(1).trim().split(/ +/g);
 const commands = args.shift().toLowerCase();
 const cmd = client.commands.get(commands) || [...client.commands.values()].find((e) => e.aliases && e.aliases.includes(commands));
-const beş_embed = new EmbedBuilder().setColor(`#2f3136`).setAuthor({name: newMessage.member.displayName, iconURL: newMessage.author.avatarURL({ dynamic: true, size: 2048 })}).setFooter({text: beş_config.clientPresence ? beş_config.clientPresence : `Beş Was Here` , iconURL: beş.avatarURL({ dynamic: true, size: 2048 })})
+const beş_embed = new EmbedBuilder().setColor(`#2f3136`).setAuthor({name: newMessage.member.displayName, iconURL: newMessage.author.avatarURL({ dynamic: true, size: 2048 })}).setFooter({text: beş_config.clientPresence ? beş_config.clientPresence : `Beş Was Here` , iconURL: message.author.avatarURL({ dynamic: true, size: 2048 })})
 if (cmd) {if(client.kanalbul(beş_config.commandLog)){client.kanalbul(beş_config.commandLog).send(`> **\`${newMessage.member.user.tag}\` Kullanıcısı <#${newMessage.channel.id}> Kanalında \`.${cmd.name}\` Komudunu Kullandı!**`)}
 cmd.execute(client, newMessage, args, beş_embed); }}
 module.exports.conf = {name: "messageUpdate"}
